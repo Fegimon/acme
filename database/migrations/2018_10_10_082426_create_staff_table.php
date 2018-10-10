c54248236cea37375af947657bb525e8511a165a
anusri21 <anusrimariyappan@gmail.com>
2018-10-10 15:44:02 +0530

    table

--- database/migrations/2018_10_10_082426_create_staff_table.php
+++ database/migrations/2018_10_10_082426_create_staff_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLoginTable extends Migration
+class CreateStaffTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateLoginTable extends Migration
      */
     public function up()
     {
-        Schema::create('login', function (Blueprint $table) {
+        Schema::create('staff', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('role_id');
+            $table->integer('user_id');
             $table->string('first_name');
             $table->string('last_name');
             $table->integer('mobile');
             $table->string('email');
-            $table->string('password');
-            $table->string('otp');
             $table->string('address');
             $table->timestamps();
         });
@@ -34,6 +32,6 @@ class CreateLoginTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('login');
+        Schema::dropIfExists('staff');
     }
 }
--- database/migrations/2018_10_10_091120_create_role_users_table.php
+++ database/migrations/2018_10_10_091120_create_role_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLoginTable extends Migration
+class CreateRoleUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,10 @@ class CreateLoginTable extends Migration
      */
     public function up()
     {
-        Schema::create('login', function (Blueprint $table) {
+        Schema::create('role_users', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('role_id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->integer('mobile');
-            $table->string('email');
-            $table->string('password');
-            $table->string('otp');
-            $table->string('address');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
@@ -34,6 +28,6 @@ class CreateLoginTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('login');
+        Schema::dropIfExists('role_users');
     }
 }
