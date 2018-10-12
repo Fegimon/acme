cbef44be9aef1b60cdcea34bcd9f425381dbc6c3
anusri21 <anusrimariyappan@gmail.com>
2018-10-12 11:16:01 +0530

    student

---
+++ database/migrations/2018_10_12_045212_create_acme-student_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAcmeStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('acme-student', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('dob');
+            $table->string('email');
+            $table->string('gender');
+            $table->integer('age');
+            $table->string('bloodgroup');
+            $table->string('religion');
+            $table->string('student_image');
+            $table->string('admission_no');
+            $table->string('admission_date');
+            $table->string('doj');
+            $table->string('school_name');
+            $table->string('school_city');
+            $table->string('school_state');
+            $table->string('school_zip');
+            $table->string('school_mobile');
+            $table->string('school_fax');
+            $table->string('school_email');
+
+            $table->string('father_name');
+            $table->string('mother_name');
+            $table->string('occupation');
+            $table->integer('father_mobile');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('acme-student');
+    }
+}
