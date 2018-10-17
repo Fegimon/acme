7b79674ee05326db64775ef7f91608aaf69c1f46
shanvicky <shan.vicky.1990@gmail.com>
2018-10-17 17:12:42 +0530

    models

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -39,5 +39,35 @@ class Payment extends Model
             return $result;
         }
     }
+    public function saveIncome($input) {        
+        $query = DB::table('acme-expense');
+        if ($input['id']) {
+    
+            $input['updated_at'] = Carbon::now()->toDateTimeString();
+            $result = $query->where([['id', $input['id']]])->update($input);
+            return $input['id'];
+            
+        } else {
+        
+            $input['created_at'] = Carbon::now()->toDateTimeString();
+            $result = $query->insertGetId($input);
+            return $result;
+        }
+    }
+    public function savePaymentdetails($input) {        
+        $query = DB::table('acme-paymentdetails');
+        if ($input['id']) {
+    
+            $input['updated_at'] = Carbon::now()->toDateTimeString();
+            $result = $query->where([['id', $input['id']]])->update($input);
+            return $input['id'];
+            
+        } else {
+        
+            $input['created_at'] = Carbon::now()->toDateTimeString();
+            $result = $query->insertGetId($input);
+            return $result;
+        }
+    }
     
 }
