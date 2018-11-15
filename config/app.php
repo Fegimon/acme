85e5c217da3583f9e1e2a979afd62ea486ac0de1
anusri21 <anusrimariyappan@gmail.com>
2018-11-15 18:47:59 +0530

    config

--- config/app.php
+++ config/app.php
@@ -174,6 +174,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Snowfire\Beautymail\BeautymailServiceProvider::class,
+
     ],
 
     /*
